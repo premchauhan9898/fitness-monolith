2cc408725ab539b9c57507f36a069b550098cb14
PREM <premchauhan9898@gmail.com>
2026-08-22 22:53:06 +0530

    created some entities and added some dependencies

---
+++ src/main/java/com/project/fitness/model/Activity.java
@@ -0,0 +1,28 @@
+package com.project.fitness.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Entity
+public class Activity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    private ActivityType type;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private Map<String, Object> additionalMatrix;
+
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/project/fitness/model/User.java
@@ -0,0 +1,24 @@
+package com.project.fitness.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+}
