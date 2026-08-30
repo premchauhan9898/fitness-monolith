6ad40fa558b378f8870bd6096b7f9b22b1b0ea37
PREM <premchauhan9898@gmail.com>
2026-08-30 21:04:54 +0530

    created controller, service and repository layers

---
+++ src/main/java/com/project/fitness/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.project.fitness.controller;
+
+
+import com.project.fitness.model.User;
+import com.project.fitness.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+    @PostMapping("/register")
+    public User register(User user) {
+        return userService.register(user);
+    }
+
+}
--- src/main/java/com/project/fitness/model/User.java
+++ src/main/java/com/project/fitness/model/User.java
@@ -2,6 +2,9 @@ package com.project.fitness.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
